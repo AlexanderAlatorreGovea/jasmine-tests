f926812343c768f53a957a7565be973fdf7811df
Alexander <Alexander_alatorre1993@hotmail.com>
2021-10-19 15:19:17 -0600

    Added new mock tests

---
+++ util.test.js
@@ -0,0 +1,9 @@
+const { loadTitle } = require('./util');
+
+//jest.mock('./http');
+
+test('should print an uppercase text', () => {
+  loadTitle().then(title => {
+    expect(title).toBe('DELECTUS AUT AUTEM');
+  });
+});
